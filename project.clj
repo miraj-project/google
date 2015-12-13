2817855a0a6483a61b410788c31341c254a75b67
Gregg Reynolds <reynolds-gregg@norc.org>
2015-12-13 03:07:02 -0600

    initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject polymer.elements/google "1.2.3-SNAPSHOT"
+  :description "Polymer Google Web Elements"
+  :url "https://github.com/mobileink/polymer.elements.google"
+  :license {:name "BSD License"
+            :url "http://polymer.github.io/LICENSE.txt"})
