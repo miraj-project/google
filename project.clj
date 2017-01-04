c602a4a41a9755ceb0a3dd2a8461bbd6a58fa247
Gregg Reynolds <reynolds-gregg@norc.org>
2017-01-04 13:13:22 -0600

    checkpoint

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/mobileink/polymer.google"
   :license {:name "EPL"
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
-    :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+    :dependencies [[org.clojure/clojure "1.8.0"]
                    [miraj/markup "0.1.0-SNAPSHOT"]])
